3bc0609679086871e25f66e4c0a364724d7f0c4e
Terra <terra.taylor@gmail.com>
2020-10-28 10:00:30 -0600

    updating instructions

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 
 Can you convert your SQL queries from yesterday to work within JS?
+Please translate the SQL queries that you wrote during your last requirement into knex queries.
+
+Please render one query per page, i.e,
+    -- A list of the top 5 countries by number of invoices
+    should have its own path in the index.js file and it's own template(.hbs file).
+
+
 
 run `npm install` then `npm start` to get started!
 
 Here's the knex website to help:  http://knexjs.org/
+And the handlebars website for templating:  https://handlebarsjs.com/guide/expressions.html#path-expressions
--- index.js
+++ index.js
@@ -2,37 +2,27 @@ const express = require('express');
 let Router = require('express-promise-router');
 const handlebars = require('express-handlebars');
 require('dotenv').config();
-if (process.env.REMOTE_DB_URL) {
-    // db Connection w/ Heroku
-    const db = require('knex')({
-      client: 'pg',
-      connection: {
-        connectionString: process.env.REMOTE_DB_URL,
-        ssl: true
-      }
-    });
-  } else {
-    // db Connection w/ localhost
-    var db = require('knex')({
-      client: 'pg',
-      connection: {
-        host: 'sql-exercises.20bits.com',
-        user: 'student',
-        password: 'sqlrocks123!',
-        database: 'sql_exercises'
-      }
-    });
+// db Connection w/ localhost
+var db = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'sql-exercises.20bits.com',
+    user: 'student',
+    password: 'sqlrocks123!',
+    database: 'sql_exercises'
   }
+});
+
 
 const port = 3000;
 const app = express();
 
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars({
-    layoutsDir: __dirname + '/views/layouts',
-    extname: 'hbs',
-    defaultLayout: 'planB',
-    partialsDir: __dirname + '/views/partials/'
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs',
+  defaultLayout: 'planB',
+  partialsDir: __dirname + '/views/partials/'
 }));
 
 app.use(express.static('public'))
@@ -40,21 +30,21 @@ app.use(express.static('public'))
 let router = Router();
 app.use(router);
 
-router.get('/', async (req, res) => {   
-    res.render('main', {layout: 'index'});
+router.get('/', async (request, response) => {
+  response.render('main', { layout: 'index' });
 });
 
 
-router.get('/albums', async (req, res) => {
-    let albums = await db.select('*').from('albums').orderBy('title', 'DESC');
-    console.log(albums);
-    res.render('albums', {layout: 'index', albums: albums, show: true});
+router.get('/albums', async (request, response) => {
+  let albums = await db.select('*').from('albums').orderBy('title', 'DESC');
+  console.log(albums);
+  response.render('albums', { layout: 'index', albums: albums, show: true });
 });
 
-router.get('/artists', async (req, res) => {
-    let artists = await db.select('*').from('artists').orderBy('name', 'DESC');
-    console.log(artists);
-    res.render('artists', {layout: 'index', artists: artists, show: true});
+router.get('/artists', async (request, response) => {
+  let artists = await db.select('*').from('artists').orderBy('name', 'DESC');
+  console.log(artists);
+  response.render('artists', { layout: 'index', artists: artists, show: true });
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
